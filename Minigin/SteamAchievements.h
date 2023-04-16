6048c1d2914418ea87beefb7cb9a54a96cb850ef
MatthiasOoms <matthias.ooms1@gmail.com>
2023-04-17 01:11:32 +0200

    Started implementing Steam Achievements

---
+++ Minigin/SteamAchievements.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <steam_api.h>
+
+namespace dae
+{
+#define _ACH_ID( id, name ) { id, #id, name, "", 0, 0 }
+	struct Achievement_t
+	{
+		int m_eAchievementID;
+		const char* m_pchAchievementID;
+		char m_rgchName[128];
+		char m_rgchDescription[256];
+		bool m_bAchieved;
+		int m_iIconImage;
+	};
+
+	class SteamAchievements
+	{
+	private:
+		int64 m_iAppID; // Our current AppID
+		Achievement_t* m_pAchievements; // Achievements data
+		int m_iNumAchievements; // The number of Achievements
+		bool m_bInitialized; // Have we called Request stats and received the callback?
+
+	public:
+		SteamAchievements(Achievement_t* Achievements, int NumAchievements);
+		~SteamAchievements();
+
+		bool RequestStats();
+		bool SetAchievement(const char* ID);
+
+		STEAM_CALLBACK(SteamAchievements, OnUserStatsReceived, UserStatsReceived_t,
+			m_CallbackUserStatsReceived);
+		STEAM_CALLBACK(SteamAchievements, OnUserStatsStored, UserStatsStored_t,
+			m_CallbackUserStatsStored);
+		STEAM_CALLBACK(SteamAchievements, OnAchievementStored,
+			UserAchievementStored_t, m_CallbackAchievementStored);
+	};
+}
+
