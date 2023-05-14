610dee2661413c2c2cebd21448b188e50ec45cac
MatthiasOoms <matthias.ooms1@gmail.com>
2023-05-15 01:51:19 +0200

    Started implementing pImpl

--- Minigin/SDLSoundSystem.cpp
+++ Minigin/SDLSoundSystem.cpp
@@ -1,22 +1,62 @@
 #include "SDLSoundSystem.h"
 
-void SDLSoundSystem::Play(const sound_id id, const float volume)
+class dae::SDLSoundSystem::SDLSoundSystemImpl
 {
+public:
+	void Play(const sound_id id, const float volume)
+	{
+
+	}
+
+	void Pause()
+	{
+
+	}
+
+	void Resume()
+	{
+
+	}
+
+	void Stop()
+	{
+
+	}
+
+	void Load(std::string filePath)
+	{
+
+	}
+};
+
+
+void dae::SDLSoundSystem::Play(const sound_id id, const float volume)
+{
+	pImpl->Play(id, volume);
 }
 
-void SDLSoundSystem::Pause()
+void dae::SDLSoundSystem::Pause()
 {
+	pImpl->Pause();
 }
 
-
-void SDLSoundSystem::Resume()
+void dae::SDLSoundSystem::Resume()
 {
+	pImpl->Resume();
 }
 
-void SDLSoundSystem::Stop()
+void dae::SDLSoundSystem::Stop()
 {
+	pImpl->Stop();
 }
 
 void dae::SDLSoundSystem::Load(std::string filePath)
 {
+	pImpl->Load(filePath);
+}
+
+dae::SDLSoundSystem::~SDLSoundSystem()
+{
+	delete pImpl;
+	pImpl = nullptr;
 }
