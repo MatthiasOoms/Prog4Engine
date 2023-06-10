3e7dbbf1c026b9465b2cf95e70239684e156b390
MatthiasOoms <matthias.ooms1@gmail.com>
2023-06-10 19:25:36 +0200

    Finished LoggingSoundSystem

--- Minigin/LoggingSoundSystem.h
+++ Minigin/LoggingSoundSystem.h
@@ -9,12 +9,12 @@ namespace dae
 	{
 		std::unique_ptr<SoundSystem> _real_ss;
 	public:
-		LoggingSoundSystem(std::unique_ptr<SoundSystem>&& ss) { _real_ss = (std::move(ss)); }
+		LoggingSoundSystem(std::unique_ptr<SoundSystem>&& ss);
 		virtual ~LoggingSoundSystem() = default;
 		void Play(const sound_id id, const float volume) override;
-		void Pause() override { _real_ss->Pause(); };
-		void Resume() override { _real_ss->Resume(); };
-		void Stop() override { _real_ss->Stop(); };
+		void Pause() override;
+		void Resume() override;
+		void Stop() override;
 		void Load(const std::string& filePath) override;
 	};
 }
